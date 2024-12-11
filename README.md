a3c3300b32ab7d65f5f7a4bc19f80a350ebc10aa
Byron Montaño <byronmb0407@gmail.com>
2024-12-10 20:36:45 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# app_car_sales_3dmodels
+ APP ui de venyta de autos con visualizacion de los autos en 3D
---
+++ android/app/src/main/kotlin/com/example/proyecto3d_2/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.proyecto3d_2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
