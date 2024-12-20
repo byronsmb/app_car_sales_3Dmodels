f9068842621dd86ae6fe34204df45e8854b3dad5
Byron Montaño <byronmb0407@gmail.com>
2024-12-19 21:02:33 -0500

    pagina principal

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -8,222 +8,47 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Model Viewer')),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Container(
-            height: MediaQuery.of(context).size.height * 0.4,
-            decoration: BoxDecoration(
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey.withOpacity(0.5),
-                  spreadRadius: 5,
-                  blurRadius: 7,
-                  offset: Offset(0, 3), // changes position of shadow
-                ),
-              ],
-              gradient: LinearGradient(
-                colors: [
-                  Colors.white,
-                  Colors.grey,
+      backgroundColor: Color.fromARGB(255, 43, 129, 250),
+      body: ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+          return Card(
+              elevation: 4.0,
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(8),
+                    child: Container(
+                      height: 200.0,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        image: DecorationImage(
+                            image: AssetImage(
+                                "assets/images/2012_chevrolet_camaro_zl1.png"),
+                            fit: BoxFit.cover),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(16.0),
+                    alignment: Alignment.centerLeft,
+                    child: Text("supportingText"),
+                  ),
+                  ButtonBar(
+                    children: [
+                      TextButton(
+                        child: const Text('CONTACT AGENT'),
+                        onPressed: () {/* ... */},
+                      ),
+                      TextButton(
+                        child: const Text('LEARN MORE'),
+                        onPressed: () {/* ... */},
+                      )
+                    ],
+                  )
                 ],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-            ),
-            child: const ModelViewer(
-              // backgroundColor: Color.fromARGB(255, 192, 23, 23),
-              src: 'assets/3d/honda_nr750_1994.glb',
-              /* - assets/3d/mersedes_cls63.glb
-   
-    
-
-     - assets/3d/1965_ford_mustang_convertible.glb */
-              alt: 'A 3D model of an astronaut',
-              ar: true,
-              autoRotate: false,
-              //iosSrc: 'https://modelviewer.dev/shared-assets/models/Astronaut.usdz',
-              disableZoom: false,
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "Mercedes Benz GT",
-                style: GoogleFonts.nunitoSans(
-                  textStyle: Theme.of(context).textTheme.displayLarge,
-                  fontSize: 22,
-                  fontWeight: FontWeight.w700,
-                  //fontStyle: FontStyle.italic,
-                ),
-              ),
-            ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Container(
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 21, 109, 231),
-                    borderRadius: BorderRadius.circular(5)),
-                height: MediaQuery.of(context).size.width * 0.15,
-                width: MediaQuery.of(context).size.width * 0.25,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      "Velocidad",
-                      style: GoogleFonts.kanit(
-                          textStyle: Theme.of(context).textTheme.displayLarge,
-                          fontSize: 14,
-                          color: Colors.white
-                          //fontWeight: FontWeight.w700,
-                          //fontStyle: FontStyle.italic,
-                          ),
-                    ),
-                    Text(
-                      "120km/h",
-                      style: GoogleFonts.kanit(
-                          textStyle: Theme.of(context).textTheme.displayLarge,
-                          fontSize: 19,
-                          color: Colors.white
-                          //fontWeight: FontWeight.w700,
-                          //fontStyle: FontStyle.italic,
-                          ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 21, 109, 231),
-                    borderRadius: BorderRadius.circular(5)),
-                height: MediaQuery.of(context).size.width * 0.15,
-                width: MediaQuery.of(context).size.width * 0.25,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      "Velocidad",
-                      style: GoogleFonts.kanit(
-                          textStyle: Theme.of(context).textTheme.displayLarge,
-                          fontSize: 14,
-                          color: Colors.white
-                          //fontWeight: FontWeight.w700,
-                          //fontStyle: FontStyle.italic,
-                          ),
-                    ),
-                    Text(
-                      "120km/h",
-                      style: GoogleFonts.kanit(
-                          textStyle: Theme.of(context).textTheme.displayLarge,
-                          fontSize: 19,
-                          color: Colors.white
-                          //fontWeight: FontWeight.w700,
-                          //fontStyle: FontStyle.italic,
-                          ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 21, 109, 231),
-                    borderRadius: BorderRadius.circular(5)),
-                height: MediaQuery.of(context).size.width * 0.15,
-                width: MediaQuery.of(context).size.width * 0.25,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Text(
-                      "Velocidad",
-                      style: GoogleFonts.kanit(
-                          textStyle: Theme.of(context).textTheme.displayLarge,
-                          fontSize: 14,
-                          color: Colors.white
-                          //fontWeight: FontWeight.w700,
-                          //fontStyle: FontStyle.italic,
-                          ),
-                    ),
-                    Text(
-                      "120km/h",
-                      style: GoogleFonts.kanit(
-                          textStyle: Theme.of(context).textTheme.displayLarge,
-                          fontSize: 19,
-                          color: Colors.white
-                          //fontWeight: FontWeight.w700,
-                          //fontStyle: FontStyle.italic,
-                          ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Container(
-              child: Text(
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-                style: GoogleFonts.sora(
-                    textStyle: Theme.of(context).textTheme.bodySmall,
-                    fontSize: 13,
-                    color: const Color.fromARGB(255, 53, 52, 52)
-                    //fontWeight: FontWeight.w700,
-                    //fontStyle: FontStyle.italic,
-                    ),
-                maxLines: 8,
-                overflow: TextOverflow.ellipsis,
-              ),
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Align(
-              alignment: Alignment.centerLeft,
-              child: Text(
-                "\$70 000.00",
-                style: GoogleFonts.kanit(
-                  textStyle: Theme.of(context).textTheme.displayLarge,
-                  fontSize: 25,
-
-                  //fontWeight: FontWeight.w700,
-                  //fontStyle: FontStyle.italic,
-                ),
-              ),
-            ),
-          ),
-          Container(
-            width: 200.0, // Set the desired width
-            height: 50.0, // Set the desired height
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(15),
-              color: Color.fromARGB(255, 21, 109, 231),
-            ),
-            child: ElevatedButton(
-              onPressed: () {
-                // Button onPressed action
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Color.fromARGB(255, 21, 109, 231),
-              ),
-              child: Text(
-                'COMPRAR',
-                style: GoogleFonts.kanit(
-                    textStyle: Theme.of(context).textTheme.displayLarge,
-                    fontSize: 20,
-                    //fontWeight: FontWeight.w700,
-                    color: Colors.white
-                    //fontStyle: FontStyle.italic,
-                    ),
-              ),
-            ),
-          ),
-          SizedBox(height: 0.5)
-        ],
+              ));
+        },
       ),
     );
   }
---
+++ lib/screens/car_detail.dart
@@ -0,0 +1,230 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:model_viewer_plus/model_viewer_plus.dart';
+
+class name extends StatelessWidget {
+  const name({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Model Viewer')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.4,
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.5),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+              gradient: LinearGradient(
+                colors: [
+                  Colors.white,
+                  Colors.grey,
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+            ),
+            child: const ModelViewer(
+              // backgroundColor: Color.fromARGB(255, 192, 23, 23),
+              src: 'assets/3d/honda_nr750_1994.glb',
+              /* - assets/3d/mersedes_cls63.glb
+   
+    
+
+     - assets/3d/1965_ford_mustang_convertible.glb */
+              alt: 'A 3D model of an astronaut',
+              ar: true,
+              autoRotate: false,
+              //iosSrc: 'https://modelviewer.dev/shared-assets/models/Astronaut.usdz',
+              disableZoom: false,
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "Mercedes Benz GT",
+                style: GoogleFonts.nunitoSans(
+                  textStyle: Theme.of(context).textTheme.displayLarge,
+                  fontSize: 22,
+                  fontWeight: FontWeight.w700,
+                  //fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 21, 109, 231),
+                    borderRadius: BorderRadius.circular(5)),
+                height: MediaQuery.of(context).size.width * 0.15,
+                width: MediaQuery.of(context).size.width * 0.25,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "Velocidad",
+                      style: GoogleFonts.kanit(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          fontSize: 14,
+                          color: Colors.white
+                          //fontWeight: FontWeight.w700,
+                          //fontStyle: FontStyle.italic,
+                          ),
+                    ),
+                    Text(
+                      "120km/h",
+                      style: GoogleFonts.kanit(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          fontSize: 19,
+                          color: Colors.white
+                          //fontWeight: FontWeight.w700,
+                          //fontStyle: FontStyle.italic,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 21, 109, 231),
+                    borderRadius: BorderRadius.circular(5)),
+                height: MediaQuery.of(context).size.width * 0.15,
+                width: MediaQuery.of(context).size.width * 0.25,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "Velocidad",
+                      style: GoogleFonts.kanit(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          fontSize: 14,
+                          color: Colors.white
+                          //fontWeight: FontWeight.w700,
+                          //fontStyle: FontStyle.italic,
+                          ),
+                    ),
+                    Text(
+                      "120km/h",
+                      style: GoogleFonts.kanit(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          fontSize: 19,
+                          color: Colors.white
+                          //fontWeight: FontWeight.w700,
+                          //fontStyle: FontStyle.italic,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    color: Color.fromARGB(255, 21, 109, 231),
+                    borderRadius: BorderRadius.circular(5)),
+                height: MediaQuery.of(context).size.width * 0.15,
+                width: MediaQuery.of(context).size.width * 0.25,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Text(
+                      "Velocidad",
+                      style: GoogleFonts.kanit(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          fontSize: 14,
+                          color: Colors.white
+                          //fontWeight: FontWeight.w700,
+                          //fontStyle: FontStyle.italic,
+                          ),
+                    ),
+                    Text(
+                      "120km/h",
+                      style: GoogleFonts.kanit(
+                          textStyle: Theme.of(context).textTheme.displayLarge,
+                          fontSize: 19,
+                          color: Colors.white
+                          //fontWeight: FontWeight.w700,
+                          //fontStyle: FontStyle.italic,
+                          ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Container(
+              child: Text(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+                style: GoogleFonts.sora(
+                    textStyle: Theme.of(context).textTheme.bodySmall,
+                    fontSize: 13,
+                    color: const Color.fromARGB(255, 53, 52, 52)
+                    //fontWeight: FontWeight.w700,
+                    //fontStyle: FontStyle.italic,
+                    ),
+                maxLines: 8,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text(
+                "\$70 000.00",
+                style: GoogleFonts.kanit(
+                  textStyle: Theme.of(context).textTheme.displayLarge,
+                  fontSize: 25,
+
+                  //fontWeight: FontWeight.w700,
+                  //fontStyle: FontStyle.italic,
+                ),
+              ),
+            ),
+          ),
+          Container(
+            width: 200.0, // Set the desired width
+            height: 50.0, // Set the desired height
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(15),
+              color: Color.fromARGB(255, 21, 109, 231),
+            ),
+            child: ElevatedButton(
+              onPressed: () {
+                // Button onPressed action
+              },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Color.fromARGB(255, 21, 109, 231),
+              ),
+              child: Text(
+                'COMPRAR',
+                style: GoogleFonts.kanit(
+                    textStyle: Theme.of(context).textTheme.displayLarge,
+                    fontSize: 20,
+                    //fontWeight: FontWeight.w700,
+                    color: Colors.white
+                    //fontStyle: FontStyle.italic,
+                    ),
+              ),
+            ),
+          ),
+          SizedBox(height: 0.5)
+        ],
+      ),
+    );
+  }
+}
