3aa386ab70d4ff0b464eb3382f861891caf56c79
Byron Montaño <byronmb0407@gmail.com>
2024-12-23 20:16:13 -0500

    Update datos_ficticios.dart

--- lib/data/datos_ficticios.dart
+++ lib/data/datos_ficticios.dart
@@ -4,6 +4,41 @@ const autosDisponibles = [
   Auto(
     titulo: "",
     descripcion: "descripcion",
-    imageUrl: "imageUrl",
+    imageUrl: "assets/images/kia_optima_k5.png",
+  ),
+  Auto(
+    titulo: "",
+    descripcion: "descripcion",
+    imageUrl: "assets/images/2012_chevrolet_camaro_zl1.png",
+  ),
+  Auto(
+    titulo: "",
+    descripcion: "descripcion",
+    imageUrl: "assets/images/2021_lamborghini_countach_lpi_800-4.png",
+  ),
+  Auto(
+    titulo: "",
+    descripcion: "descripcion",
+    imageUrl: "assets/images/caminhao_volvo_fh_750.png",
+  ),
+  Auto(
+    titulo: "",
+    descripcion: "descripcion",
+    imageUrl: "assets/images/honda_nr750_1994.png",
+  ),
+  Auto(
+    titulo: "",
+    descripcion: "descripcion",
+    imageUrl: "assets/images/suzuki_sx4_sportback.png",
+  ),
+  Auto(
+    titulo: "",
+    descripcion: "descripcion",
+    imageUrl: "assets/images/tesla_roadster_2.png",
+  ),
+  Auto(
+    titulo: "",
+    descripcion: "descripcion",
+    imageUrl: "assets/images/1965_ford_mustang_convertible.png",
   ),
 ];
