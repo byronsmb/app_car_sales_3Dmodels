bcaab99312d9751c99647890587b8f6b4e231b50
Byron Montaño <byronmb0407@gmail.com>
2025-01-03 18:49:42 -0500

    Update datos_ficticios.dart

--- lib/data/datos_ficticios.dart
+++ lib/data/datos_ficticios.dart
@@ -9,22 +9,26 @@ const autosDisponibles = [
   ),
   Auto(
     titulo: "2012 Chevrolet Camaro ZL1",
-    descripcion: "Ya está aquí el Camaro de serie más potente de la Historia. Su motor 6.2 LSA small block entrega 580 CV de potencia y un par máximo brutal: 754 Nm. Está sobrealimentado por compresor volumétrico de tipo Roots, con una capacidad de 1,9 litros. Entrega 150 CV más que el legendario 1969 Camaro ZL1 7.0.",
+    descripcion:
+        "Ya está aquí el Camaro de serie más potente de la Historia. Su motor 6.2 LSA small block entrega 580 CV de potencia y un par máximo brutal: 754 Nm. Está sobrealimentado por compresor volumétrico de tipo Roots, con una capacidad de 1,9 litros. Entrega 150 CV más que el legendario 1969 Camaro ZL1 7.0.",
     imageUrl: "assets/images/2012_chevrolet_camaro_zl1.png",
   ),
   Auto(
     titulo: "Lamborghini Countach LPI 800-4",
-    descripcion: "Cincuenta años después de su presentación en el Salón del Automóvil de Ginebra, el legendario Lamborghini Countach vuelve a ser protagonista con una serie limitada híbrida que conmemora el diseño visionario que revolucionó para siempre el panorama de los superdeportivos modernos.",
+    descripcion:
+        "Cincuenta años después de su presentación en el Salón del Automóvil de Ginebra, el legendario Lamborghini Countach vuelve a ser protagonista con una serie limitada híbrida que conmemora el diseño visionario que revolucionó para siempre el panorama de los superdeportivos modernos.",
     imageUrl: "assets/images/2021_lamborghini_countach_lpi_800-4.png",
   ),
   Auto(
-    titulo: "",
-    descripcion: "descripcion",
+    titulo: "Volvo FH 750",
+    descripcion:
+        "Con la tecnología de vanguardia de Volvo y un diseño aerodinámico, este camión maximiza el ahorro de combustible y minimiza las emisiones, reduciendo el impacto ambiental mientras mantiene un rendimiento excepcional.",
     imageUrl: "assets/images/caminhao_volvo_fh_750.png",
   ),
   Auto(
-    titulo: "",
-    descripcion: "descripcion",
+    titulo: "Honda NR 750",
+    descripcion:
+        "La Honda NR 750 destaca por su diseño elegante y futurista para la época, con un característico escape cuádruple y una estética refinada en rojo brillante, el color distintivo de Honda Racing.",
     imageUrl: "assets/images/honda_nr750_1994.png",
   ),
   Auto(
